ce81e85c193e64eaea8f91c4888845783be77510
Betsinat Amare <amarebetsinat@gmail.com>
2025-08-08 22:42:14 +0300

    added basics

--- Frontend/Expense_Tracker/src/App.jsx
+++ Frontend/Expense_Tracker/src/App.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Home from "./pages/Dashboared/Home";
+import Income from "./pages/Dashboared/Income";
+import Expense from "./pages/Dashboared/Expense";
 
-const App = () => {
-  return (
+import { div } from "three/tsl";
+const App = () =>{
+  return(
     <div>
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" exact element ={<Login />} />
+          <Route path="/signup" exact element ={<SignUp />} />
+          <Route path="/dashboard" exact element ={<Home />} />
+          <Route path="/income" exact element ={<Income />} /><Route path="/Expense" exact element ={<Expense />} />
+        </Routes>
+      </Router>
     </div>
-  )
-}
+  );
+};
+export default App;
 
-export default App
+
+const Root = () => {
+
+// Check if token exists in localStorage
+  const isAuthenticated = !!localStorage.getItem("token");
+  // Redirect to dashboard if authenticated, otherwise to login
+    return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to = "/login" />
+  );
+};
